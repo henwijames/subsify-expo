ca655993b9fa5254adbd9e3b25d91032cd56007f
Henry James Ribano <114987518+henwijames@users.noreply.github.com>
2026-06-23 11:40:06 +0400

    feat: implement fonts.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,23 +8,21 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-      <Text className="text-xl font-bold text-red-500">
-        Welcome to Nativewind!
-      </Text>
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
       >
-        Go to onboarding
+        Go to Onboarding
       </Link>
       <Link
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
         href="/(auth)/sign-up"
       >
         Go to Sign Up
       </Link>
       <Link
-        className="mt-4 rounded bg-primary text-white p-4"
+        className="mt-4 rounded bg-primary text-white p-4 font-sans-bold"
         href="/(auth)/sign-in"
       >
         Go to Sign In
